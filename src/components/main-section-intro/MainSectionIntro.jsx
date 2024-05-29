252faa76413badad731d92cf40293ee0448de928
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-05-29 10:18:15 +0200

    finished intro image

--- src/components/main-section-intro/MainSectionIntro.jsx
+++ src/components/main-section-intro/MainSectionIntro.jsx
@@ -1,10 +1,21 @@
+// Images
+import IntroImage from '../../assets/illustration-hero.svg';
+
 // Styles
 import './main-section-intro.css';
 
 function MainSectionIntro() {
   return (
     <section className='section intro'>
-        Main section intro
+      <div className='text-container'>
+        tekst
+      </div>
+      <div className='image-container'>
+        <div className='image-wrapper'>
+          <img src={IntroImage} alt='Abstract computer screen' />
+          <div className='background-shape'></div>
+        </div>
+      </div>
     </section>
   );
 }
