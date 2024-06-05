53c14f65db68a1994073e350e215ed77cbd42cf3
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-06-05 10:46:15 +0200

    fixed image being cut off by container

--- src/components/image/Image.jsx
+++ src/components/image/Image.jsx
@@ -4,10 +4,12 @@ import './image.css';
 function Image({ image }) {
     return (
         <div className='image-wrapper'>
-            <img
-                src={image}
-                alt='Intro image'
-            />
+            <div className='image-container'>
+                <img
+                    src={image}
+                    alt='Intro image'
+                />
+            </div>
             <div className='background-shape'></div>
         </div>
     );
